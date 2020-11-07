e990c20a24997053725295b2fdcb3dfb16638dbf
RisenFromAshes <ashrafurkhan37@gmail.com>
2020-11-07 20:19:52 +0600

    make my changes

--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -1,27 +1,33 @@
+"use strict";
+
 var vidElm;
 var darker;
+
 function posVid() {
   var sw = window.innerWidth;
   var sh = window.innerHeight;
   var vw = 1920;
   var vh = 1080;
-  if ((sw/sh) <= (vw/vh)) {
+
+  if (sw / sh <= vw / vh) {
     vh = sh;
-    vw = (vh * 16) / 9;
+    vw = vh * 16 / 9;
   } else {
     vw = sw;
-    vh = (vw * 9) / 16;
+    vh = vw * 9 / 16;
   }
-  vidElm.style.marginLeft = (sw-vw) / 2 + "px";
+
+  vidElm.style.marginLeft = (sw - vw) / 2 + "px";
   darker.style.height = vh + "px";
   vidElm.style.width = vw + "px";
 }
 
-window.onload = () => {
+window.onload = function () {
   darker = document.querySelector("#darker");
   vidElm = document.querySelector("#bg-video");
   posVid();
 };
-window.onresize = () => {
+
+window.onresize = function () {
   posVid();
 };
